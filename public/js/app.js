d77f78fefdd8ab01eeef68cb140f5c890e9e0af3
Thomas Wallner <thomas.wallner23@gmail.com>
2022-01-04 11:33:33 +0100

    uploader done

--- public/js/app.js
+++ public/js/app.js
@@ -19,6 +19,22 @@ Vue.createApp({
             .catch(console.log);
     },
     methods: {
-        clickHandler() {},
+        clickHandler() {
+            const fd = new FormData();
+            fd.append("title", this.title);
+            fd.append("username", this.username);
+            fd.append("description", this.description);
+            fd.append("file", this.file);
+            fetch("/upload", {
+                method: "POST",
+                body: fd,
+            })
+                .then((res) => res.json())
+                .then(console.log) // same as (result) => console.log(result)
+                .catch(console.log); // same as (err) => console.log(err)
+        },
+        fileSelectHandler(e) {
+            this.file = e.target.files[0];
+        },
     },
 }).mount("#main");
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
+const { uploader } = require("./uploads");
 const { getImages } = require("./db");
 
 app.use(express.static("./public"));
-
 app.use(express.json());
+
+app.post("/upload", uploader.single("file"), (req, res) => {
+    console.log(req.body);
+    console.log(req.file);
+});
 
 app.get("/images", (req, res) => {
     getImages()
